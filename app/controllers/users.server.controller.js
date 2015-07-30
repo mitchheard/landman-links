478cba4c29d69f86f4a02b240d17a4654faa54f1
mitchheard <mitchheard@gmail.com>
2015-07-29 22:18:43 -0500

    added CRUD for users

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -28,6 +28,7 @@ exports.list = function(req, res, next) {
 exports.read = function(req, res) {
   res.json(req.user);
 };
+
 // middleware populating the req.user object
 // will use to deal with manipulation of single documents when performing CRUD operations
 exports.userByID = function(req, res, next, id) {
@@ -44,4 +45,26 @@ exports.userByID = function(req, res, next, id) {
       }
     }
   );
+};
+
+exports.update = function(req, res, next) {
+  User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+    if(err) {
+      return next(err);
+    }
+    else {
+      res.json(user);
+    }
+  });
+};
+
+exports.delete = function(req, res, next) {
+  req.user.remove(function(err) {
+    if (err) {
+      return next(err);
+    }
+    else {
+      res.json(req.user);
+    }
+  })
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -3,5 +3,12 @@ var users = require('../../app/controllers/users.server.controller');
 module.exports = function(app) {
   app.route('/users').post(users.create).get(users.list);
 
-  app.route('/users')
-}
+  app.route('/users/:userId').get(users.read).put(users.update);
+
+  app.route('/users/:userId').get(users.read).put(users.update).delete(users.delete);
+  // handles the population of the req.user object
+  // defines a middleware to be executed before any other middleware that uses that parameter
+  // users.userByID will be executed before any other middleware,
+  // registered with the userId param, is this case is the users.read() middleware
+  app.param('userId', users.userByID);
+};
